c859f101ed01e66bc6f7774c791697873e870cb4
bala <bala223344@gmail.com>
2020-03-31 20:21:35 +0530

    card connect api fixed

--- config/initializers/cardconnect.rb
+++ config/initializers/cardconnect.rb
@@ -6,5 +6,5 @@ CardConnect.configure do |config|
   #config.endpoint = 'https://fts.cardconnect.com:6443'
   config.api_username = 'cardconnect1'
   config.api_password = 'XDk94@13L001CD01x'
-  config.endpoint = 'https://fts.cardconnect.com:8443/'
+  config.endpoint = 'https://fts-uat.cardconnect.com/'
 end
