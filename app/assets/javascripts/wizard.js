1abfa99f0d31d31aa5a75193fd7d90cce51794a1
bala <bala223344@gmail.com>
2020-10-31 07:51:16 +0530

    deleted js

--- app/assets/javascripts/wizard.js
+++ app/assets/javascripts/wizard.js
@@ -1,9 +1,4 @@
-//= require pace
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require bootstrap-sprockets
+
 //= require functions
 //= require form_wizard
-//= require bootstrap-wizard
 
