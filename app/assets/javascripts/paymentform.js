7aab16538f94e4e7251b6c0296aa8c59c4ed27b3
bala <bala223344@gmail.com>
2020-12-22 20:57:33 +0530

    form completed

--- app/assets/javascripts/paymentform.js
+++ app/assets/javascripts/paymentform.js
@@ -1,5 +1,6 @@
 //= require paymentform/jquery
 //= require paymentform/jquery-steps 
+//= require paymentform/bootstrap
 //= require paymentform/main
 //= require paymentform/inputs
 //= require paymentform/imask
