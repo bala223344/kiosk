319e19fd892b7433cb7564c1b6a55844ed829f61
bala <bala223344@gmail.com>
2021-02-07 13:34:44 +0000

    scott changes

--- app/assets/javascripts/paymentform.js
+++ app/assets/javascripts/paymentform.js
@@ -1,8 +1,9 @@
 //= require paymentform/jquery
-//= require paymentform/jquery-steps 
+//= require paymentform/jquery-steps
 //= require paymentform/bootstrap
 //= require jquery.inputmask.min
 //= require paymentform/main
 //= require paymentform/inputs
 //= require paymentform/imask
 //= require paymentform/script
+//= require paymentform/lottie-player
